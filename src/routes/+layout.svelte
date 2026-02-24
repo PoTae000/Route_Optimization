e36174cb0ea5bfc2818938865069029bd96b55ed
PoTae000 <ngo81559@gmail.com>
2026-02-24 15:17:11 +0700

    edit_detail

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
 	import './layout.css';
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { inject } from '@vercel/analytics';
 
 	injectSpeedInsights();
+	inject();
 
 	let { children } = $props();
 </script>
